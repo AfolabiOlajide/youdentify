56d54b78d9708b94e7e4f5f50bfe6b4a4b1088f9
Afolabi Olajide Samuel <samolajide20@gmail.com>
2024-12-17 22:00:42 +0100

    initial project setup

---
+++ lib/constants.tsx
@@ -0,0 +1,17 @@
+import { createThirdwebClient, defineChain, getContract } from "thirdweb";
+
+export const APP_NAME = "YouDentify";
+export const APP_DESCRIPTION =
+    "YouDentify - Decentralized Identity Platform on the mantle blockchain";
+
+export const chain = defineChain(1029);
+export const contractAddress = "0x4f87A34cAA93e0A377dCF609A1F0Fb354633647B";
+export const client = createThirdwebClient({
+    clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
+});
+
+export const contract = getContract({
+    client,
+    address: contractAddress,
+    chain: chain,
+});
