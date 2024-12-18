ec346541a1d6f2cee71de183e4348540b7254454
Afolabi Olajide Samuel <samolajide20@gmail.com>
2024-12-18 04:47:06 +0100

    added hero section

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,10 +1,12 @@
 import NavBar from '@/components/navbar/NavBar'
 import React from 'react'
+import HeroSection from './HeroSection'
 
 const HomePage = () => {
   return (
     <div>
       <NavBar />
+      <HeroSection />
     </div>
   )
 }
--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -6,17 +6,18 @@ import ActionButtons from "./_components/ActionButton";
 
 const NavBar = () => {
     const navbarClasses = `
-        flex items-center justify-between space-x-10 bg-white  h-14
-        sticky top-0 z-50 border-b border-gray-200
+        bg-white py-[1rem] sticky top-0 z-50 border-b border-gray-200
     `;
 
     return (
         <div className={navbarClasses}>
-            <div className="flex items-center justify-center">
-                <Logo />
-                <Menu />
+            <div className="flex items-center justify-between space-x-10  cont">
+                <div className="flex items-center justify-center">
+                    <Logo />
+                    <Menu />
+                </div>
+                <ActionButtons />
             </div>
-            <ActionButtons />
         </div>
     );
 };
