8d1c6f6d468a7c949ab9f068703e67640e0fe5cc
Afolabi Olajide Samuel <samolajide20@gmail.com>
2024-12-18 15:42:22 +0100

    fixed bugs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ["images.unsplash.com", "assets.aceternity.com"],
+    },
+    webpack: (config) => {
+        config.externals.push(
+            "pino-pretty" /* add any other modules that might be causing the error */
+        );
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
